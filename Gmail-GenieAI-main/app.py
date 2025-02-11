f33d25d925518265b9f39762b8376f8a826eac7a
Mathimaran1 <113694577+Mathimaran1@users.noreply.github.com>
2025-02-11 16:11:30 +0530

    Delete Gmail-GenieAI-main directory

--- Gmail-GenieAI-main/app.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-from gmail_api import main  # Import your main function
-
-st.title("Smart AI Chat")
-main()  # Run your Gmail API script
--- Gmail-GenieAI-main/auth.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-# Testing secret retrieval
-st.write(f"GEMINI API Key: {st.secrets['GEMINI_API_KEY']}")
-st.write(f"Gmail Client ID: {st.secrets['GMAIL_OAUTH_CREDS']['client_id']}")
